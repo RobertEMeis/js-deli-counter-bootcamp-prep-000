a440cc591b9300ad89af8889e2221602e50756a7
Robert Meis <robert.meis@colorado.edu>
2017-10-30 21:37:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name) {
 
   var position = katzDeliLine.length+1;
 
-  katzDeli.push("Welcome, " + name + "." + " You are number " + position + " in line.")
+  return ("Welcome, " + name + "." + " You are number " + position + " in line.")
 
-return katzDeli
+
 }
