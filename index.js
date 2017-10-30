ad836dcd5609783509d4e275c05901cbae9e053f
Robert Meis <robert.meis@colorado.edu>
2017-10-30 21:37:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
 
-  var position = katzDeli.length+1;
+  var position = katzDeliLine.length+1;
 
   katzDeli.push("Welcome, " + name + "." + " You are number " + position + " in line.")
 
